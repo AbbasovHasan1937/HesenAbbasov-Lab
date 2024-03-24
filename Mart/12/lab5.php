83d807a47b886bb82992ebdbd2e46938c030c615
Hasan <abbasovhesen704@gmail.com>
2024-03-24 13:40:02 -0700

    lab 4 ve lab5

--- Mart/12/lab5.php
+++ Mart/12/lab5.php
@@ -1,28 +1,31 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
-
-$x = 30;
-$y = 30;
- 
-$cem = $x + $y;  
-$ferq = $x - $y;
-$vurma = $x * $y;
-$bolme = $y / $x;  
- 
-echo "Cem: " . $cem . "\n"; 
-
-echo "Ferq: " . $ferq . "\n";
-
-echo "Vurma: " . $vurma . "\n"; 
-
-echo "Bolme: " . $bolme;
- 
-?>
-
-
-
-</body>
-</html>
+    $num1 = 10;
+    $num2 = 20;
+    $operator = "+";
+   // $operator = "-";
+   // $operator = "*";
+   // $operator = "/";
+    $result = '';
+            switch ($operator) {
+                case '+':
+                    $result = $num1 + $num2;
+                    break;
+                case '-':
+                    $result = $num1 - $num2;
+                    break;
+                case '*':
+                    $result = $num1 * $num2;
+                    break;
+                case '/':
+                    if ($num2 != 0) { 
+                        $result = $num1 / $num2;
+                    } else {
+                        $result = '0-a bölmek olmaz!';
+                    }
+                    break;
+                default:
+                    $result = 'bele bir operator movcud deyil!';
+                    break;
+            }
+            echo "Cavab: $result";
+    ?>
